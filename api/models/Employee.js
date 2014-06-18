075c61c4131c0448ddccc48bade3aee22da7642e
paulomid <paulomid@vmokshagroup.com>
2014-06-18 12:42:43 +0530

    password

--- api/models/Employee.js
+++ api/models/Employee.js
@@ -6,37 +6,35 @@
  * @description :: A short summary of how this model works and what it represents.
  * @docs		:: http://sailsjs.org/#!documentation/models
  */
- var _randString = require('random-string');
- var easycrypto = require('easycrypto').getInstance();
- module.exports = {
+ //var _randString = require('random-string');
+ //var easycrypto = require('easycrypto').getInstance();
+module.exports = {
 
-     attributes: {
-
-         toJSON: function () {
-             // this gives you an object with the current values
-             var obj = this.toObject();
-             delete obj.password;
-             delete obj.confirmPassword;
-             return obj;
-         }
-
-     },
-     beforeCreate: function (values, next) {
-     console.log("value = " + values.code); 
-     var randomString = require('random-string');
-     values.code = randomString({ length: 6 })        
-     next();
-     }
-     /*beforeCreate: function (obj, cb) {
+    attributes: {
+    },
+    beforeCreate: function (values, next) {
+        var randomString = require('random-string');
+        values.code = randomString({ length: 6 });
+        console.log("value = " + values.code);
+        //Added by Paulomi: start
+        var easycrypto = require('easycrypto').getInstance();
+        var encryptedPassword = easycrypto.encrypt(values.password, 'mypassword'); //"mypassword" -> is the password to be given during decryption        
+        values.password = encryptedPassword;
+        values.confirmPassword = encryptedPassword;
+        console.log("password = " + values.password);
+        //Added by Paulomi: stop
+        next();//Added by Paulomi: In your Employee model, you need to use the next() method. This method passes control back to the next piece of middleware on the stack. Without it, it blocks execution.
+    }
+    /*beforeCreate: function (obj, cb) {
          
-         var encryptedPassword = easycrypto.encrypt(obj.password, 'mypassword'); //"mypassword" -> is the password to be given during decryption
-         obj.password = encryptedPassword
-         obj.confirmPassword = encryptedPassword
-         console.log("Employee before create")
-         obj.code = _randString({ length: 6 });
-         console.log(obj + "" + obj.code + " ," + obj.password);
-         //next();
-         return cb(null, obj);
-     }*/
- };
+    var encryptedPassword = easycrypto.encrypt(obj.password, 'mypassword'); //"mypassword" -> is the password to be given during decryption
+    obj.password = encryptedPassword
+    obj.confirmPassword = encryptedPassword
+    console.log("Employee before create")
+    obj.code = _randString({ length: 6 });
+    console.log(obj + "" + obj.code + " ," + obj.password);
+    //next();
+    return cb(null, obj);
+    }*/
+};
 

b84d9b04949aa92a864f8369cefefeef510170f3
paulomid <paulomid@vmokshagroup.com>
2014-08-22 12:14:46 +0530

    api for attendance by date range

--- api/controllers/EmployeeAttendanceController.js
+++ api/controllers/EmployeeAttendanceController.js
@@ -1,20 +1,39 @@
-var _ = require("lodash");
-module.exports = _.merge(_.cloneDeep(require("../services/BaseController")),{
-    
+/**
+ * EmployeeAttendanceController.js 
+ *
+ * @description ::
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
 
+module.exports = {
 
+    attendanceByDateRange: function (req, res) {
+        console.log("get employees by date range")
+        var params = req.params.all();
+        var employees = [];
+        EmployeeAttendance.find({ EmpId: params.EmpId }, function (err, employeeattendance) {
+            if (err) {
+                return console.log(err);
+            } else {
+                if (employeeattendance === undefined) {
+                    res.send(500, "Retrieveing data failed....")
+                } else {
+                    employeeattendance.forEach(function (item) {
+                        item.AttendanceDate = new Date(item.AttendanceDate);
+                    });
+                    //var employees = (from i in employeeattendance
+                    //where (i.AttendanceDate >= new Date(params.start_date) && i.AttendanceDate <= new Date(params.end_date)) );
 
-  /**
-   * Overrides for the settings in `config/controllers.js`
-   * (specific to CasestudyController)
-   */
-  _config: {
-      /*blueprints: {
-            actions: true,
-            rest: true,
-            shortcuts: true
-        }*/
-  }
+                    employeeattendance.forEach(function (item) {
+                        item.AttendanceDate = new Date(item.AttendanceDate);
+                        if (item.AttendanceDate >= new Date(params.start_date) && item.AttendanceDate <= new Date(params.end_date)) {
+                            employees.push(item);
+                        }
+                    });                    
+                    res.send(200, employees);
+                }
+            }
+        });
+    }
 
-  
-});
+}
